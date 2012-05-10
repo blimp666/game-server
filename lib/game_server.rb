bc86cc863edca48dfc2f297dfc0b3ae17d4e37c6
blimp666 <skyburg@mail.ru>
2012-05-10 18:56:59 +0400

    Version bump to 0.1.4

--- lib/game_server.rb
+++ lib/game_server.rb
@@ -1,5 +1,5 @@
-require  'base_listner' #File.dirname(__FILE__)+'/base_listner'
-require  'server_starter' #File.dirname(__FILE__)+'/server_starter'
 module GameServer
 end
+require  'base_listner' #File.dirname(__FILE__)+'/base_listner'
+require  'server_starter' #File.dirname(__FILE__)+'/server_starter'
 
