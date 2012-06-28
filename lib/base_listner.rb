e6dde8da9253edba89447aa357faffce27507452
Peter Rezikov <crazypit13@gmail.com>
2012-06-28 16:37:39 +0400

    new logger

--- lib/base_listner.rb
+++ lib/base_listner.rb
@@ -3,16 +3,12 @@ require File.dirname(__FILE__) + '/daemon_logger'
 # Базовый модуль для прослушивания JSON запросов от клиента
 module GameServer::BaseListner
 
-  include DaemonLogger
-  def log_file_path
-    Rails.root + 'log/socket.log'
-  end
-
+  include DaemonLogger::Mixins
 
   def find_controller(request)
     return unless request.name =~ /^[A-Za-z_]+$/
     x = (controllers_classes_root + "::" + request.name.camelize)
-    logger.info x
+    log x
     x.constantize
   rescue NameError
     return
@@ -41,14 +37,13 @@ module GameServer::BaseListner
   end
 
   def send_data(data)
-    logger.info "Data sended to #{connection_info}: " + data#.inspect
+    log "Data sended to #{connection_info}: " + data#.inspect
     super(data)
   end
 
 
   def critical_error(e)
-    logger.error e
-    logger.error e.backtrace.join("\n")
+    log_exception e
     send_error(e.class.to_s.underscore)
 #    close_connection_after_writing
   end
@@ -56,14 +51,13 @@ module GameServer::BaseListner
   # Обработать входящие данные (может быть несклько строк запросов в одном пакете данных)
   def receive_data(data)
     data.gsub!("\000", "")
-    logger.info "Received from #{connection_info}: " + data.inspect
+    log "Received from #{connection_info}: " + data.inspect
     return if policy_file_request(data)
     data.split("\n").each do |query_string|
       process_query(query_string)
     end
   rescue => e
     critical_error(e)
-    logger.info "\n\n\n\n\n\nCRITICAL RECEIVE DATA SERVER ERROR"
   end
 
   # Обработать один запрос
@@ -79,7 +73,6 @@ module GameServer::BaseListner
   rescue GameError => e
     send_error(e.class.to_s.underscore)
   rescue => e
-    logger.info "\n\n\n\n\n\nCRITICAL SERVER ERROR"
     critical_error(e)
   end
 
--- lib/daemon_logger.rb
+++ lib/daemon_logger.rb
@@ -1,17 +1,64 @@
-module DaemonLogger
-  def init_logger
-    return if @logger
-    file = File.open(log_file_path, 'a')
+class DaemonLogger
+  include Singleton
+  attr_writer :logger
+  
+  class NullLogger
+    def method_missing(*args, &block)
+      p @args
+    end
+  end
+  
+  
+  def init(params)
+    raise TypeError.new(":log_file_path required") unless params[:log_file_path]
+    file = File.open(params[:log_file_path], 'a')
     file.sync = true
-    @logger = Logger.new(file)
-    @logger.level = Logger::DEBUG
-    @logger.formatter = Logger::Formatter.new
-    @logger.datetime_format = "%y-%m-%d %H:%M:%S.%L"
+    self.logger = Logger.new(file)
+    logger.level = Logger::DEBUG
+    logger.formatter = Logger::Formatter.new
+    logger.datetime_format = "%y-%m-%d %H:%M:%S.%L"
+  end
+  
+  def self.init(*params)
+    instance.init(*params)
   end
 
   def logger
-    init_logger
-    @logger
+    if @logger
+      @logger
+    else
+      warn 'Logger not initialized yet, run init with :log_file_path' 
+      NullLogger.new
+    end
+  end
+  
+  module Mixins
+    
+    def log(message, log_type = :info)
+      puts "#{Time.now} #{log_type} #{message}"
+      DaemonLogger.instance.logger.send(log_type, message)
+    end
+    
+    def log_exception(exception, send_mail = true, additional_info = "")
+      message = <<"EOF"
+Exception was raised #{exception}
+#{additional_info}
+
+BACKTRACE:
+#{exception.backtrace.join("\n")}
+EOF
+      puts "SENDING MAIL" if send_mail
+      log(message, :error)
+    end
+    
+    def with_exception_logging
+      yield
+    rescue => e
+      log_exception e, send_mail = true, additional_info = 'WITH EXCEPTION LOGGING CRITICAL ERROR'
+      raise e
+    end
+    
+    
   end
 
 end
