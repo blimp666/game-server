5e47ec8622729da0cf59f2e0f3ec9dde3cf0abdd
V_M <nobody@nowhere.com>
2012-12-28 12:59:49 +0400

    notification via exception_notification

--- lib/daemon_logger.rb
+++ lib/daemon_logger.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 require 'net/smtp'
 require 'singleton'
 require 'logger'
@@ -6,14 +7,14 @@ class DaemonLogger
   include Singleton
   attr_writer :logger
   attr_accessor :email_receivers, :smtp_server
-  
+
   class NullLogger
     def method_missing(*args, &block)
       p @args
     end
   end
-  
-  
+
+
   # ==== Params
   # params<Hash>::
   # log_file_path<String>:: куда будет писаться лог
@@ -29,7 +30,7 @@ class DaemonLogger
     self.email_receivers = params[:email_receivers].to_a
     self.smtp_server = params[:smtp_server] || 'localhost'
   end
-  
+
   def self.init(*params)
     instance.init(*params)
   end
@@ -38,50 +39,53 @@ class DaemonLogger
     if @logger
       @logger
     else
-      warn 'Logger not initialized yet, run init with :log_file_path' 
+      warn 'Logger not initialized yet, run init with :log_file_path'
       NullLogger.new
     end
   end
-  
-  def send_emails_to_receivers(message)
-    return if !email_receivers or email_receivers.empty?
-    Net::SMTP.start(smtp_server) do |smtp|
-      smtp.send_message message, 'daemon.exceptions@skyburg.com', email_receivers
-    end
-  rescue => e
-    log_exception(e, false, "SENDING EXCEPTION EMAIL ERROR")
-  end
-  
-  
+
+  # def send_emails_to_receivers(message)
+  #   return if !email_receivers or email_receivers.empty?
+
+  #   Net::SMTP.start(smtp_server) do |smtp|
+  #     smtp.send_message message, 'daemon.exceptions@skyburg.com', email_receivers
+  #   end
+  # rescue => e
+  #   log_exception(e, false, "SENDING EXCEPTION EMAIL ERROR")
+  # end
+
+
   module Mixins
-    
+
     # FIXME:: DOCUMENTATION
     def log(message, log_type = :info)
       puts "#{Time.now} #{log_type} #{message}"
       DaemonLogger.instance.logger.send(log_type, message)
     end
-    
-    def log_exception(exception, send_mail = true, additional_info = "")
-      message = <<"EOF"
-Exception was raised #{exception}
-#{additional_info}
 
-BACKTRACE:
-#{exception.backtrace.join("\n")}
-EOF
-      DaemonLogger.instance.send_emails_to_receivers(message) if send_mail
+    def log_exception(exception, send_mail = true, additional_info = "")
+
+#       message = <<"EOF"
+# Exception was raised #{exception}
+# #{additional_info}
+
+# BACKTRACE:
+# #{exception.backtrace.join("\n")}
+# EOF
+#       DaemonLogger.instance.send_emails_to_receivers(message) if send_mail
       log(message, :error)
+      ExceptionNotifier::Notifier.background_exception_notification(e).deliver
     end
-    
-    
+
+
     def with_exception_logging
       yield
     rescue => e
       log_exception e, send_mail = true, additional_info = 'WITH EXCEPTION LOGGING CRITICAL ERROR'
       raise e
     end
-    
-    
+
+
   end
 
   include Mixins
